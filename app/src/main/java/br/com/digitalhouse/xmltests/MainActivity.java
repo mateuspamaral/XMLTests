15b6f68fc627a08d9b6b3699bc903d517618ea0d
Mateus Pessoa Amaral <mateuspa@gmail.com>
2019-05-07 10:54:04 -0300

    Adicionando mensagem de log a cada hora que a aplicação muda de estado, onCreate, onStart, onResume, onPause, onStop, onDestroy.

--- app/src/main/java/br/com/digitalhouse/xmltests/MainActivity.java
+++ app/src/main/java/br/com/digitalhouse/xmltests/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.digitalhouse.xmltests;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("LOG", "onCreate()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i("LOG", "onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i("LOG", "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i("LOG", "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.i("LOG", "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i("LOG", "onDestroy()");
     }
 }
